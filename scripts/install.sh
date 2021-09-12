a101660df9568d8e12dcf508745383d020ef8bed
AdamMiltonBarker <adammiltonbarker@leukemiaresearchassociation.ai>
2021-09-12 15:51:54 +0200

    Resolves #5 Complete installation scripts

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
-PRN="HIAS Historical Data Interface"
-FMSG="- $PRN installation terminated"
+FMSG="HIAS Historical Data Interface component installation terminated!"
 
-read -p "? This script will install the $PRN on your device. Are you ready (y/n)? " cmsg
+printf -- 'This script will install the HIAS Historical Data Interface component on HIAS Core.\n';
+printf -- '\033[33m WARNING: This is an inteteractive installation, please follow instructions provided. \033[0m\n';
 
-if [ "$cmsg" = "Y" -o "$cmsg" = "y" ]; then
-	echo "- Installing $PRN"
-	echo "- $PRN installed!"
+read -p "Proceed (y/n)? " proceed
+if [ "$proceed" = "Y" -o "$proceed" = "y" ]; then
+	printf -- 'Installing the HIAS Historical Data Interface component....\n';
+	conda install flask
+	conda install -c conda-forge paho-mqtt
+	conda install pandas
+	conda install psutil
+	conda install pymongo
+	conda install requests
+	conda install urllib3
+	pip install mgoquery
+	printf -- '\033[32m SUCCESS: HIAS Historical Data Interface component installed successfully! \033[0m\n';
+	exit 0
 else
 	echo $FMSG;
-	exit
+	exit 1
 fi
